e427d7a7d1bf6cf8065aee7988d25f2436b7f7a4
sxomile <milosprorocic1307@gmail.com>
2025-04-19 21:21:41 +0200

    Added token cap and destroy function

--- sxom-token/contracts/SxomToken.sol
+++ sxom-token/contracts/SxomToken.sol
@@ -3,9 +3,24 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract SxomToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("SxomToken", "SXT"){
-        _mint(msg.sender, initialSupply);
+contract SxomToken is ERC20Capped {
+    
+    address payable public owner;
+
+    constructor() ERC20("SxomToken", "SXT") ERC20Capped(5000000 * (10 ** decimals())){
+        owner = payable(msg.sender);
+        _mint(owner, 5000000 * (10 ** decimals()));
     }
+
+    function destroy() public onlyOwner() {
+        selfdestruct(owner);
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only owner can make this call!");
+        _;
+    }
+
 }
